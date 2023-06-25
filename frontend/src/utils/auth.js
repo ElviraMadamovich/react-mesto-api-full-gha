b884c9635f979bc5f280b04c8bc86f151284b6fd
ElviraMadamovich <110635987+ElviraMadamovich@users.noreply.github.com>
2023-06-25 22:16:07 +0300

    Update auth.js

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,45 +1,53 @@
 import { authUrl } from './baseUrl';
 
-const request = ({
-  url,
-  method = 'POST',
-  token,
-  data
-}) => {
-  return fetch(`${authUrl}${url}`, {
+const request = (url, method, data, token) => {
+  const options = {
     method,
     headers: {
-      'Content-type': 'application/json',
-      ...!!token && { 'authorization': `Bearer ${token}` }
+      'Content-Type': 'application/json',
     },
-    ...!!data && { body: JSON.stringify(data) }
-  })
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      return Promise.reject(res);
+  };
+
+  if (data) {
+    options.body = JSON.stringify(data);
+  }
+  if (token) {
+    options.headers = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    };
+  }
+
+  return fetch(`${authUrl}/${url}`, options).then((res) => {
+    if (!res.ok) {
+      throw new Error(`Ошибка: ${res.status}`);
+    }
+    return res.json();
+  });
+};
+
+export const register = (email, password) => {
+  return request('signup',
+    'POST',
+    {
+      password: `${password}`,
+      email: `${email}`,
     });
 }
 
-export const register = (email, password) => {
-  return request({
-    url: '/signup',
-    data: { password: password, email: email }
-  });
-}
-
 export const authorize = (email, password) => {
-  return request({
-    url: '/signin',
-    data: { password, email }
-  });
+  return request('signin',
+    'POST',
+    {
+      password: `${password}`,
+      email: `${email}`,
+    });
 }
 
 export const checkToken = (token) => {
-  return request({
-    url: '/users/me',
-    method: 'GET',
-    token
-  });
+  return request('users/me',
+    'GET',
+    null,
+    token);
 }
